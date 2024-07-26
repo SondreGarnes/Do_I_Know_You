31630729113b67db9b2f88ad7e05269b618abf35
Sondre <sondre433@gmail.com>
2024-07-26 13:08:33 +0200

    Added login view and other views

--- diky_project/diky_app/serializers.py
+++ diky_project/diky_app/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 
 class UserSerializer(serializers.Serializer):
-    name=serializers.CharField()
+    first_name=serializers.CharField()
+    last_name=serializers.CharField()
     email=serializers.EmailField()
+    password=serializers.CharField(write_only=True)
+
+class LoginSerializer(serializers.Serializer):
+    email=serializers.EmailField()
+    password=serializers.CharField(write_only=True)
